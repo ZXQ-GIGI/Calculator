e837763173781470b6c870c214be503e2e6e514b
gigi <snailzhao1995@163.com>
2017-08-14 22:16:50 +0800

    update readme

--- test.js
+++ test.js
@@ -3,25 +3,16 @@ var readline = require('readline');
 var Calculator = require('./index.js');
 var num = '';
 
-//var expression = "-3+5*(12^4-4)-(-6)/2";
-
-//var result = new Calculator(expression);
-//console.log(result.calculate());
 var rl = readline.createInterface({
 	input:process.stdin,
 	output:process.stdout
 });
 
-//rl.question("...",function(answer){
-	
-//	rl.close();
-//});
 rl.on("line",function(line){
-	var result = new Calculator(line.trim());
 	switch(line.trim()){
 		case 'e':rl.close();
 		default: 
-			result.expression = line.trim().replace("ans",num);
+			var result = new Calculator(line.trim().replace("ans",num));
 			console.log(result.calculate());
 			num = result.result;
 	}

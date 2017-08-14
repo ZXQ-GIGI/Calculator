c35840c7efc8e06f5aef1f34f5aafb3b68c926bd
gigi <snailzhao1995@163.com>
2017-08-14 19:39:23 +0800

    finish calculate

--- index.js
+++ index.js
@@ -37,32 +37,34 @@ Calculator.prototype.isBracket = function(data) {
 	return false;
 };
 
-Calculator.prototype.getWeight = function(data) {
+Calculator.prototype.getPriority = function(data) {
 	var ret = -1;
 	switch(data){
 		case OPERATOR_ADD:
-			ret = 0;
-			break;
 		case OPERATOR_SUB:
 			ret = 1;
 			break;
 		case OPERATOR_MUL:
+		case OPERATOR_DIV:
 			ret = 2;
 			break;
-		case OPERATOR_DIV:
-			ret = 3;
-			break;
 		case OPERATOR_POW:
-			ret = 4;
-			break;
-		case LEFT_BRACKET:
-			ret = 5;
-			break;
-		case RIGHT_BRACKET:
-			ret = 6;
-			break;
+			ret = 3;
+			break;	
 	}
 	return ret;
+};
+
+Calculator.prototype.isGreat = function(op1, op2) {
+	if(this.getPriority(op1) < 0 || this.getPriority(op2) < 0){
+		throw new Error("unexpected operator.");
+	}
+	if(this.getPriority(op1) > this.getPriority(op2)){
+		return true;
+	}
+	else{
+		return false;
+	}
 };
 Calculator.prototype.isValid = function() {
 	if(this.expression == null){
@@ -92,14 +94,14 @@ Calculator.prototype.isValid = function() {
 		throw new Error("Expression error: missing ')'.");
 	}
 };
-Calculator.prototype.string2Array = function() {
+Calculator.prototype.getExpression = function() {
 	this.isValid();
 	var arr = new Array();
 	arr.push(LEFT_BRACKET);
 	for(var i = 0; i < this.expression.length; i++){
 		if(this.isOperator(this.expression[i]) || this.isBracket(this.expression[i])){
 			if(this.expression[i] == OPERATOR_SUB && arr[arr.length - 1] == LEFT_BRACKET){
-				arr.push("0");
+				arr.push(0);
 			}
 			arr.push(this.expression[i]);
 		}else{
@@ -110,18 +112,79 @@ Calculator.prototype.string2Array = function() {
 			}
 		}
 	}
+	for(var i = 0; i < arr.length; i++){
+		if(!this.isOperator(arr[i]) && !this.isBracket(arr[i])){
+			arr[i] = parseFloat(arr[i]);
+		}
+	}
 	arr.push(RIGHT_BRACKET);
 	return arr;
 };
-
-function Node(data, left, right){
-	this.data = data;
-	this.left = left;
-	this.right = right;
-}
-function BinaryTree(array){
-	this.root = new Node(null,null,null);
-}
-BinaryTree.prototype.createTree = function() {
-	// body...
+Calculator.prototype.getReversePolish = function() {
+	var expression = this.getExpression();
+	var opStack = new Array();
+	var rpnQueuelist = new Array();
+	for(var i = 0; i < expression.length; i++){
+		if(typeof(expression[i]) == "number"){
+			rpnQueuelist.push(expression[i]);
+		}
+		else{
+			if(opStack.length == 0 || expression[i] == LEFT_BRACKET){
+				opStack.push(expression[i].slice());
+			}
+			else if(expression[i] == RIGHT_BRACKET){
+				while(opStack[opStack.length - 1] != LEFT_BRACKET){
+					rpnQueuelist.push(opStack.pop());
+				}
+				opStack.pop();
+			}
+			else if(this.isOperator(expression[i])){
+				while(opStack.length != 0){	
+					if(opStack[opStack.length - 1] == LEFT_BRACKET ||
+					    this.isGreat(expression[i],opStack[opStack.length - 1])){
+						opStack.push(expression[i]);
+						break;
+					}
+					else{
+						rpnQueuelist.push(opStack.pop());
+					}
+				}
+			}
+		}
+	}
+	return rpnQueuelist;
 };
+
+Calculator.prototype.calculate = function() {
+	var rpnQueuelist = this.getReversePolish();
+	var tempStack = new Array();
+	for(var i = 0; i < rpnQueuelist.length; i++){
+		if(typeof(rpnQueuelist[i]) == "number"){
+			tempStack.push(rpnQueuelist[i]);
+		}
+		else{
+			var num1 = tempStack.pop();
+			var num2 = tempStack.pop();
+			tempStack.push(Count(num2,num1,rpnQueuelist[i]));
+		}
+	}
+	return tempStack[0];
+};
+
+function Count(num1, num2, operator){
+	switch(operator){
+		case OPERATOR_ADD:
+			return (num1 + num2);
+		case OPERATOR_SUB:
+			return (num1 - num2);
+		case OPERATOR_MUL:
+			return (num1 * num2);
+		case OPERATOR_DIV:
+			return (num1 / num2);
+		case OPERATOR_POW:
+			return Math.pow(num1,num2);
+	}
+	return false;
+}
+
+
--- test.js
+++ test.js
@@ -1,7 +1,22 @@
 'use strict'
+var readline = require('readline');
 var Calculator = require('./index.js');
 
-var expression = "-3+5*(1.2-4)-6/2";
+//var expression = "-3+5*(12^4-4)-(-6)/2";
 
-var result = new Calculator(expression);
-console.log(result.string2Array());
+//var result = new Calculator(expression);
+//console.log(result.calculate());
+var rl = readline.createInterface({
+	input:process.stdin,
+	output:process.stdout
+});
+
+rl.question("Please input:",function(answer){
+	var result = new Calculator(answer);
+	console.log(result.calculate());
+	rl.close();
+});
+
+rl.on("close",function(){
+	process.exit(0);
+});

0847e4b295a28642f9a7efa40419cff738b9fca1
gigi <snailzhao1995@163.com>
2017-08-15 19:30:00 +0800

    add sin/cos/tan/ln/lg

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const OPERATOR_COS = "cos";
 const OPERATOR_TAN = "tan";
 const OPERATOR_LN = "ln";
 const OPERATOR_LG = "lg";
-const OPERATOR_LOG = "log";
+//const OPERATOR_LOG = "log";
 
 
 function Calculator(arithm_expression){
@@ -28,6 +28,12 @@ Calculator.prototype.isOperator = function(data) {
 		case OPERATOR_MUL:
 		case OPERATOR_DIV:
 		case OPERATOR_POW:
+			return true;
+	}
+	return false;
+};
+Calculator.prototype.isUnaryOperator = function(data) {
+	switch(data){
 		case OPERATOR_SIN:
 		case OPERATOR_COS:
 		case OPERATOR_TAN:
@@ -55,40 +61,43 @@ Calculator.prototype.getPriority = function(data) {
 			break;
 		case OPERATOR_MUL:
 		case OPERATOR_DIV:
+		case OPERATOR_SIN:
+		case OPERATOR_COS:
+		case OPERATOR_TAN:
+		case OPERATOR_LN:
+		case OPERATOR_LG:
 			ret = 2;
 			break;
 		case OPERATOR_POW:
 			ret = 3;
-			break;
-		case OPERATOR_SIN:
-		case OPERATOR_COS:
-		case OPERATOR_TAN:
-			ret = 4;
 			break;	
 	}
 	return ret;
 };
 
 Calculator.prototype.isGreat = function(op1, op2) {
-	if(this.getPriority(op1) > this.getPriority(op2)){
-		return true;
-	}
-	else{
-		return false;
-	}
+	//if(this.getPriority(op1) < 0 || this.getPriority(op2) < 0){
+	//	throw new Error("222");
+	//}
+	//if(this.getPriority(op1) > this.getPriority(op2)){
+	return this.getPriority(op1) - this.getPriority(op2);
+	//}
 };
 Calculator.prototype.isValid = function() {
 	var num = 0;
 	for(var i = 0; i < this.expression.length; i++){
 		if(i > 0 && i < this.expression.length - 1){
-			if(this.isOperator(this.expression[i]) 
-				&& this.isOperator(this.expression[i-1])){
+			if(this.isOperator(this.expression[i]) &&
+				 (this.isOperator(this.expression[i-1]) 
+				|| this.isUnaryOperator(this.expression[i-1]))){
 				markError(this.expression, i);
 				console.log("Error: '" + this.expression[i]  + "' is invalid.");
 				this.error = true;
 			}
 		}
-		if(i == this.expression.length - 1 && this.isOperator(this.expression[i])){
+		if(i == this.expression.length - 1 && 
+			(this.isOperator(this.expression[i]) 
+			|| this.isUnaryOperator(this.expression[i]))){
 			markError(this.expression, i);
 			console.log("Error: '" + this.expression[i]  + "' is invalid.");
 			this.error = true;
@@ -121,8 +130,18 @@ Calculator.prototype.getExpression = function() {
 				arr.push(0);
 			}
 			arr.push(this.expression[i]);
-		}else{
-			if(this.isOperator(arr[arr.length - 1]) || this.isBracket(arr[arr.length - 1])){
+		}
+		else if(/^[a-z]+$/.test(this.expression[i])){
+			if(/^[a-z]+$/.test(arr[arr.length - 1])){
+				arr[arr.length - 1] += this.expression[i];
+			}else{
+				arr.push(this.expression[i]);
+			}
+		}
+		else{
+			if(this.isOperator(arr[arr.length - 1]) 
+				|| this.isBracket(arr[arr.length - 1])
+				|| this.isUnaryOperator(arr[arr.length - 1])){
 				arr.push(this.expression[i]);
 			}else{
 				arr[arr.length - 1] += this.expression[i];
@@ -130,7 +149,9 @@ Calculator.prototype.getExpression = function() {
 		}
 	}
 	for(var i = 0; i < arr.length; i++){
-		if(!this.isOperator(arr[i]) && !this.isBracket(arr[i])){
+		if(!this.isOperator(arr[i]) 
+			&& !this.isBracket(arr[i]) 
+			&& !this.isUnaryOperator(arr[i])){
 			arr[i] = parseFloat(arr[i]);
 		}
 	}
@@ -142,6 +163,7 @@ Calculator.prototype.getReversePolish = function() {
 	var opStack = new Array();
 	var rpnQueuelist = new Array();
 	for(var i = 0; i < expression.length; i++){
+		//console.log(i+"-----"  + expression[i]);
 		if(typeof(expression[i]) == "number"){
 			rpnQueuelist.push(expression[i]);
 		}
@@ -155,10 +177,15 @@ Calculator.prototype.getReversePolish = function() {
 				}
 				opStack.pop();
 			}
-			else if(this.isOperator(expression[i])){
+			else if(this.isOperator(expression[i]) || this.isUnaryOperator(expression[i])){
 				while(opStack.length != 0){	
-					if(opStack[opStack.length - 1] == LEFT_BRACKET ||
-					    this.isGreat(expression[i],opStack[opStack.length - 1])){
+					if(opStack[opStack.length - 1] == LEFT_BRACKET 
+						|| this.isGreat(expression[i],opStack[opStack.length - 1]) > 0){
+						opStack.push(expression[i]);
+						break;
+					}
+					else if(this.isGreat(expression[i],opStack[opStack.length - 1]) == 0 
+						&& this.isUnaryOperator(expression[i])){
 						opStack.push(expression[i]);
 						break;
 					}
@@ -169,6 +196,7 @@ Calculator.prototype.getReversePolish = function() {
 			}
 		}
 	}
+	console.log(rpnQueuelist);
 	return rpnQueuelist;
 };
 
@@ -179,17 +207,21 @@ Calculator.prototype.calculate = function() {
 		if(typeof(rpnQueuelist[i]) == "number"){
 			tempStack.push(rpnQueuelist[i]);
 		}
-		else{
+		else if(this.isOperator(rpnQueuelist[i])){
 			var num1 = tempStack.pop();
 			var num2 = tempStack.pop();
-			tempStack.push(Count(num2,num1,rpnQueuelist[i]));
+			tempStack.push(BinocularCount(num2,num1,rpnQueuelist[i]));
+		}
+		else{
+			var num = tempStack.pop();
+			tempStack.push(UnaryCount(num,rpnQueuelist[i]));
 		}
 	}
 	this.result = tempStack[0];
 	return tempStack[0];
 };
 
-function Count(num1, num2, operator){
+function BinocularCount(num1, num2, operator){
 	switch(operator){
 		case OPERATOR_ADD:
 			return (num1 + num2);
@@ -203,6 +235,21 @@ function Count(num1, num2, operator){
 			return Math.pow(num1,num2);
 	}
 	return false;
+}
+
+function UnaryCount(num, operator){
+	switch(operator){
+		case OPERATOR_SIN:
+			return Math.sin(num/180*Math.PI);
+		case OPERATOR_COS:
+			return Math.cos(num/180*Math.PI);
+		case OPERATOR_TAN:
+			return Math.tan(num/180*Math.PI);
+		case OPERATOR_LN:
+			return Math.log(num);
+		case OPERATOR_LG:
+			return Math.log(num)/Math.log(10);
+	}
 }
 
 function markError(expression, errorPos){

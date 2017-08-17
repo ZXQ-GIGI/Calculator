5d60eb30b8dd26cd8024c42f8917d4628c644d9c
gigi <snailzhao1995@163.com>
2017-08-17 19:21:09 +0800

    update markError

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const OPERATOR_COS = "cos";
 const OPERATOR_TAN = "tan";
 const OPERATOR_LN = "ln";
 const OPERATOR_LG = "lg";
-//const OPERATOR_LOG = "log";
-
+const OPERATOR_LOG = "log";
+const OPERATOR_COMMA = ",";
 
 function Calculator(arithm_expression){
 	this.expression = arithm_expression || null;
@@ -39,6 +39,8 @@ Calculator.prototype.isUnaryOperator = function(data) {
 		case OPERATOR_TAN:
 		case OPERATOR_LN:
 		case OPERATOR_LG:
+		case OPERATOR_LOG:
+		case OPERATOR_COMMA:
 			return true;
 	}
 	return false;
@@ -68,7 +70,7 @@ Calculator.prototype.getPriority = function(data) {
 		case OPERATOR_LG:
 		case OPERATOR_POW:
 			ret = 2;
-			break;	
+			break;
 	}
 	return ret;
 };
@@ -76,25 +78,43 @@ Calculator.prototype.getPriority = function(data) {
 Calculator.prototype.isGreat = function(op1, op2) {
 	return this.getPriority(op1) - this.getPriority(op2);
 };
+
+//Verify the rationality of the expression
+//Basic operators(+-*/^) can not appear adjacent to each other
+//Parentheses must appear in pairs
+//The first and last expressions of an expression can not be an operator
+//(except '-' for the first)
 Calculator.prototype.isValid = function() {
 	var num = 0;
 	for(var i = 0; i < this.expression.length; i++){
-		if(i > 0 && i < this.expression.length - 1){
-			if(this.isOperator(this.expression[i]) &&
-				 (this.isOperator(this.expression[i-1]) 
-				|| this.isUnaryOperator(this.expression[i-1]))){
-				markError(this.expression, i);
-				console.log("Error: '" + this.expression[i]  + "' is invalid.");
+		if(1 == i){
+			if(this.expression[1] == OPERATOR_MUL ||
+				 this.expression[1] == OPERATOR_DIV ||
+				 this.expression[1] == OPERATOR_POW){
+				markError(this.expression, 1, "invalid");
 				this.error = true;
 			}
 		}
-		if(i == this.expression.length - 1 && 
-			(this.isOperator(this.expression[i]) 
-			|| this.isUnaryOperator(this.expression[i]))){
-			markError(this.expression, i);
-			console.log("Error: '" + this.expression[i]  + "' is invalid.");
-			this.error = true;
+		if(i > 0 && i < this.expression.length - 1){
+			if(this.isOperator(this.expression[i]) &&
+				 (this.isOperator(this.expression[i-1]) || this.isUnaryOperator(this.expression[i-1]))){
+				markError(this.expression, i, "invalid");
+				this.error = true;
+			}
 		}
+		if(i == this.expression.length - 1 ){
+			if(this.isOperator(this.expression[i]) || this.isUnaryOperator(this.expression[i])){
+				markError(this.expression, i, "invalid");
+				this.error = true;
+			}
+		}
+		if(!this.isOperator(this.expression[i]) &&
+			 !this.isBracket(this.expression[i]) &&
+			 !this.isUnaryOperator(this.expression[i]) &&
+		 	 !/^[0-9]+$/.test(this.expression[i])){
+				 markError(this.expression, i, "invalid");
+				 this.error = true;
+			 }
 		if(this.expression[i] == LEFT_BRACKET){
 			num++;
 		}
@@ -102,27 +122,28 @@ Calculator.prototype.isValid = function() {
 			num--;
 		}
 		if(num < 0){
-			markError(this.expression, i);
-			console.log("Error: missing '('!");
+			markError(this.expression, i, LEFT_BRACKET);
 			this.error = true;
 		}
 	}
 	if(num > 0){
-		markError(this.expression, this.expression.length - 1);
-		console.log("Error: missing ')'!");
+		markError(this.expression, this.expression.length - 1, RIGHT_BRACKET);
 		this.error = true;
 	}
 };
+
 Calculator.prototype.getExpression = function() {
-	this.isValid();
 	var arr = new Array();
+	//console.log(this.expression);
 	arr.push(LEFT_BRACKET);
 	for(var i = 0; i < this.expression.length; i++){
 		if(this.isOperator(this.expression[i]) || this.isBracket(this.expression[i])){
 			if(this.expression[i] == OPERATOR_SUB && arr[arr.length - 1] == LEFT_BRACKET){
 				arr.push(0);
 			}
-			arr.push(this.expression[i]);
+			if(this.expression[i] != OPERATOR_ADD || arr[arr.length - 1] != LEFT_BRACKET){
+				arr.push(this.expression[i]);
+			}
 		}
 		else if(/^[a-z]+$/.test(this.expression[i])){
 			if(/^[a-z]+$/.test(arr[arr.length - 1])){
@@ -131,37 +152,31 @@ Calculator.prototype.getExpression = function() {
 				arr.push(this.expression[i]);
 			}
 		}
-		else{
-			if(this.isOperator(arr[arr.length - 1]) 
-				|| this.isBracket(arr[arr.length - 1])
-				|| this.isUnaryOperator(arr[arr.length - 1])){
+		else if(this.expression[i] != ' '){
+			if(this.isOperator(arr[arr.length - 1]) ||
+			 	 this.isBracket(arr[arr.length - 1]) ||
+				 this.isUnaryOperator(arr[arr.length - 1])){
 				arr.push(this.expression[i]);
 			}else{
 				arr[arr.length - 1] += this.expression[i];
 			}
 		}
 	}
+	arr.push(RIGHT_BRACKET);
+	this.expression = arr;
+	this.isValid();
 	for(var i = 0; i < arr.length; i++){
-		if(!this.isOperator(arr[i]) && !this.isBracket(arr[i]) 
+		if(!this.isOperator(arr[i]) && !this.isBracket(arr[i])
 			&& !this.isUnaryOperator(arr[i])){
-			var num = arr[i];
-			if(isNaN(num)){
-				arr.shift();
-				markError(arr.join(''), i-1);
-				console.log(i-1);
-				console.log("Error: '" + arr[i-1]  + "' is invalid.");
-				this.error = true;
-				return '';
-			}else{
 				arr[i] = parseFloat(arr[i]);
-			}
 		}
 	}
-	arr.push(RIGHT_BRACKET);
+	console.log(arr);
 	return arr;
 };
 Calculator.prototype.getReversePolish = function() {
 	var expression = this.getExpression();
+	//console.log(expression);
 	var opStack = new Array();
 	var rpnQueuelist = new Array();
 	for(var i = 0; i < expression.length; i++){
@@ -180,13 +195,13 @@ Calculator.prototype.getReversePolish = function() {
 				opStack.pop();
 			}
 			else if(this.isOperator(expression[i]) || this.isUnaryOperator(expression[i])){
-				while(opStack.length != 0){	
-					if(opStack[opStack.length - 1] == LEFT_BRACKET 
+				while(opStack.length != 0){
+					if(opStack[opStack.length - 1] == LEFT_BRACKET
 						|| this.isGreat(expression[i],opStack[opStack.length - 1]) > 0){
 						opStack.push(expression[i]);
 						break;
 					}
-					else if(this.isGreat(expression[i],opStack[opStack.length - 1]) == 0 
+					else if(this.isGreat(expression[i],opStack[opStack.length - 1]) == 0
 						&& this.isUnaryOperator(expression[i])){
 						opStack.push(expression[i]);
 						break;
@@ -254,17 +269,27 @@ function UnaryCount(num, operator){
 	}
 }
 
-function markError(expression, errorPos){
-	console.log(expression);
-	var arr = new Array();
-	for(var i = 0; i < expression.length; i++){
-		if(i == errorPos){
-			arr[i] = "^";
+function markError(expression, errorPos, errorType){
+	var expr = expression.slice(1,expression.length-1);
+	var str = '';
+	for(var i = 0; i <= expr.length; i++){
+		if(i == errorPos-1){
+			str += "^";
 			break;
 		}
-		arr[i] = " ";	
+		var temp = typeof(expr[i]) == 'number' ? expr[i].toString() : expr[i];
+		for(var j = 0; j < temp.length; j++){
+			str += " ";
+		}
 	}
-	console.log(arr.join(""));
+	console.log(expr.join('') + '\n' + str);
+	if(errorType == 'invalid'){
+		console.log("Error: '" + expression[errorPos]  + "' is invalid.");
+	}
+	if(errorType == LEFT_BRACKET){
+		console.log("Error: missing '('!");
+	}
+	if(errorType == RIGHT_BRACKET){
+		console.log("Error: missing ')'!");
+	}
 }
-
-
